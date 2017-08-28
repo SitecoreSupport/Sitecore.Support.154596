948921e218151cd0b20ed81228d504aad5d0e915
DK\mao <mao@sitecore.net>
2017-08-28 15:16:32 +0300

    Set last processing timestamp inside EventDisabler

---
+++ src/Sitecore.Support.154596/Data/Eventing/SqlServerEventQueue.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.DataProviders.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Eventing;
+using Sitecore.Data.Events;
+
+namespace Sitecore.Support.Data.Eventing
+{
+    public class SqlServerEventQueue : Sitecore.Data.Eventing.SqlServerEventQueue
+    {
+        public SqlServerEventQueue(SqlDataApi api) : base(api)
+        {
+        }
+
+        public SqlServerEventQueue(SqlDataApi api, Database database) : base(api, database)
+        {
+        }
+
+        protected override void SetTimestampForLastProcessing(TimeStamp currentTimestamp)
+        {
+            using (new EventDisabler())
+            {
+                base.SetTimestampForLastProcessing(currentTimestamp);
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.154596/Data/SqlServer/SqlServerDataProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Eventing;
+using Sitecore.Support.Data.Eventing;
+
+namespace Sitecore.Support.Data.SqlServer
+{
+    public class SqlServerDataProvider : Sitecore.Data.SqlServer.SqlServerDataProvider
+    {
+        public SqlServerDataProvider(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override EventQueue GetEventQueue()
+        {
+            return new SqlServerEventQueue(base.Api, base.Database);
+        }
+    }
+}
